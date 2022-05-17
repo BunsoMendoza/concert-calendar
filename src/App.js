bc2b950b269be8824286dc3c5b56082395292441
BunsoMendoza <bunsomendoza@gmail.com>
2022-05-17 16:48:47 -0700

    added empty calendar and about pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import ConcertsPage from "./components/Events/ConcertsPage";
 import Banner from "./components/Banner"
+import About from "./components/About/About";
+import Calendar from "./components/Calendar";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -20,8 +22,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
            <Route path="/concerts" element={<ConcertsPage />} />
-          {/*<Route path="/about" element={<About />} />
-          <Route path="/calendar" element={<Calendar />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/calendar" element={<Calendar />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       
---
+++ src/components/Calendar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import Footer from "./Footer";
+
+
+
+function Calendar() {
+  return (
+    <div>
+      <Container fluid className="calendar-page">
+
+        <Container className="calendar-content">
+
+          <h1 className="project-heading">
+            <strong className="blue">Calendar Header</strong>
+          </h1>
+
+          
+
+        </Container>
+
+      </Container>
+      <Footer />
+    </div>
+  );
+}
+export default Calendar;
