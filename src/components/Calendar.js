4f7ee7bb996df1eaebf97151a87f0581a156de90
BunsoMendoza <bunsomendoza@gmail.com>
2022-05-22 15:24:04 -0700

    reverting before queries added

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import * as queries from '../graphql/queries';
+
 import Footer from "./Footer";
 
 function Calendar() {
-  const allTodos = await API.graphql({ query: queries.listTodos });
-  console.log(allTodos);
+ 
 
   return (
     <div>
