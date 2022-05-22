ed2a8f6a24b70d072b6a959b832cdc0733b9da85
BunsoMendoza <bunsomendoza@gmail.com>
2022-05-22 15:18:25 -0700

    queries fix

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import * as queries from './graphql/queries';
+import * as queries from '../graphql/queries';
 import Footer from "./Footer";
 
 function Calendar() {
   const allTodos = await API.graphql({ query: queries.listTodos });
   console.log(allTodos);
-  
+
   return (
     <div>
       <Container fluid className="calendar-page">
