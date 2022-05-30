e162ccd3e0ac05a4c3e996f182667f46cd7342ef
BunsoMendoza <bunsomendoza@gmail.com>
2022-05-30 15:23:13 -0700

    touched up some styles for pagninated list

--- src/components/Events/ConcertPaginatedList.js
+++ src/components/Events/ConcertPaginatedList.js
@@ -65,74 +65,83 @@ function ConcertPaginatedList(props) {
     );
     return (
       <div>
-        
-          <Container className="concert-list-content">
-            <div className="list-group-item" id="list-item">
-              <Row>
-                <Col md="3">
-                  <strong>Event</strong>
-                </Col>
-                <Col md="2">
-                  <strong>Location</strong>
-                </Col>
-                <Col md="1">
-                  <strong>Date</strong>
-                </Col>
-                <Col md="1">
-                  <strong>Time</strong>
-                </Col>
-                <Col md="2">
-                  <strong>Venue</strong>
-                </Col>
-                <Col md="2">
-                  <strong>Scheduled Performers</strong>
-                </Col>
-                <Col md="1"></Col>
-              </Row>
-            </div>
+        <div className="top-paginated-buttons">
+          <h4>
+            Page {currentPage} of {numberOfPages}
+          </h4>
+          <button onClick={() => decrementPageNumber()}>Previous</button>
+          <button onClick={() => incrementPageNumber()}>Next</button>
+        </div>
 
-            {list.map((item) => {
-              return (
-                <li
-                  href="#"
-                  className="list-group-item list-group-item-action"
-                  id="list-item"
-                >
-                  <Row>
-                    <Col md="3">{item.displayName.split(" (")[0]}</Col>
-                    <Col md="2">
-                      {item.location["city"].substring(
-                        0,
-                        item.location["city"].length - 4
-                      )}
-                    </Col>
-                    <Col md="1">
-                      {new Date(item.start.datetime).toLocaleDateString()}
-                    </Col>
-                    <Col md="1">{item.start.time}</Col>
-                    <Col md="2">{item.venue.displayName}</Col>
-                    <Col md="2">{item.performance}</Col>
-                    <Col md="1">
-                      <a
-                        onClick={(e) => {
-                          handleAddClick(e, item);
-                        }}
-                        style={{ color: "blue" }}
-                      >
-                        Add
-                      </a>
-                    </Col>
-                  </Row>
-                </li>
-              );
-            })}
-          </Container>
-       
-        <button onClick={() => decrementPageNumber()}>Previous</button>
-        <button onClick={() => incrementPageNumber()}>Next</button>
-        <h4>
-          Page {currentPage} of {numberOfPages}
-        </h4>
+        <Container className="concert-list-content">
+          <div className="list-group-item" id="list-item">
+            <Row>
+              <Col md="3">
+                <strong>Event</strong>
+              </Col>
+              <Col md="2">
+                <strong>Location</strong>
+              </Col>
+              <Col md="1">
+                <strong>Date</strong>
+              </Col>
+              <Col md="1">
+                <strong>Time</strong>
+              </Col>
+              <Col md="2">
+                <strong>Venue</strong>
+              </Col>
+              <Col md="2">
+                <strong>Scheduled Performers</strong>
+              </Col>
+              <Col md="1"></Col>
+            </Row>
+          </div>
+
+          {list.map((item) => {
+            return (
+              <li
+                href="#"
+                className="list-group-item list-group-item-action"
+                id="list-item"
+              >
+                <Row>
+                  <Col md="3">{item.displayName.split(" (")[0]}</Col>
+                  <Col md="2">
+                    {item.location["city"].substring(
+                      0,
+                      item.location["city"].length - 4
+                    )}
+                  </Col>
+                  <Col md="1">
+                    {new Date(item.start.datetime).toLocaleDateString()}
+                  </Col>
+                  <Col md="1">{item.start.time}</Col>
+                  <Col md="2">{item.venue.displayName}</Col>
+                  <Col md="2">{item.performance}</Col>
+                  <Col md="1">
+                    <a
+                      onClick={(e) => {
+                        handleAddClick(e, item);
+                      }}
+                      style={{ color: "blue" }}
+                    >
+                      Add
+                    </a>
+                  </Col>
+                </Row>
+              </li>
+            );
+          })}
+        </Container>
+
+        <div className="bottom-paginate-buttons">
+          <button onClick={() => decrementPageNumber()}>Previous</button>
+          <button onClick={() => incrementPageNumber()}>Next</button>
+          <h4>
+            Page {currentPage} of {numberOfPages}
+          </h4>
+        </div>
       </div>
     );
   }
---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const Toast = () => {
+    return (
+        <div className="notification-container">
+            <div className="notification toast">
+    <button>
+        X
+    </button>
+    <div className="notification-image">
+        <img src="" alt="" />
+    </div>
+    <div>
+        <p className="notification-title">Toast</p>
+        <p className="notification-message">kewl</p>
+    </div>
+</div>
+        </div>
+    )
+}
+export default Toast;
